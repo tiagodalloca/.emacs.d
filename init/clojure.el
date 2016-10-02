8f85d352a52dca9a2925427108dd563c8a56d4fa
Tiago Dall'Oca <tdalloca@gmail.com>
2016-10-02 19:01:16 -0300

    no flycheck

--- init/clojure.el
+++ init/clojure.el
@@ -5,9 +5,12 @@
 (when (maybe-require-package 'cider)
   (setq nrepl-popup-stacktraces nil))
 
-(when (maybe-require-package 'flycheck-clojure)
-  (eval-after-load 'flycheck '(flycheck-clojure-setup))
-  (add-hook 'after-init-hook #'global-flycheck-mode))
+; (when (maybe-require-package 'flycheck-clojure)
+;   (eval-after-load 'flycheck '(flycheck-clojure-setup))
+;   (add-hook 'after-init-hook #'global-flycheck-mode)
+;   (when (maybe-require-package 'flycheck-pos-tip)
+;     (eval-after-load 'flycheck
+;       '(setq flycheck-display-errors-function #'flycheck-pos-tip-error-messages))))
 
 
 (add-to-list 'load-path "~/.emacs.d/vendor/parinfer-mode/")
@@ -15,11 +18,14 @@
 (add-hook 'clojure-mode-hook #'parinfer-mode)
 (add-hook 'emacs-lisp-mode-hook #'parinfer-mode)
 
-; (when (maybe-require-package 'highlight-parentheses)
-;   (require 'highlight-parentheses)
-;   (add-hook 'clojure-mode-hook #'highlight-parentheses)
-;   (add-hook 'emacs-lisp-mode-hook #'highlight-parentheses)
-;   (add-hook 'cider-repl-mode-hook #'high))
+(when (maybe-require-package 'highlight-parentheses)
+  (require 'highlight-parentheses)
+  (add-hook 'clojure-mode-hook #'highlight-parentheses)
+  (add-hook 'emacs-lisp-mode-hook #'highlight-parentheses)
+  (add-hook 'cider-repl-mode-hook #'highlight-parentheses))
+
+(when (maybe-require-package 'clojure-snippets)
+  (require 'clojure-snippets))
 
 (add-to-list 'auto-mode-alist '("\\.boot\\'" . clojure-mode))
 
--- init/sublimity.el
+++ init/sublimity.el
@@ -1,9 +1,4 @@
 (when (maybe-require-package 'sublimity)
   (require 'sublimity)
   (require 'sublimity-scroll)
-  (require 'sublimity-map)
-
-  (add-hook 'sublimity-map-setup-hook
-    (lambda ()
-      (setq buffer-face-mode-face '(:family "Monospace"))
-      (buffer-face-mode))))
+  (require 'sublimity-map))
