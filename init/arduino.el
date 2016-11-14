e3a11e1911174d536e286d2f847df6448dbf79a9
Tiago Dall'Oca <tdalloca@gmail.com>
2016-11-14 00:57:44 -0200

    Arduino file with use-package

--- init/arduino.el
+++ init/arduino.el
@@ -2,8 +2,10 @@
   (progn
     (setq auto-mode-alist (cons '("\\.\\(pde\\|ino\\)$" . arduino-mode) auto-mode-alist))
     (autoload 'arduino-mode "arduino-mode" "Arduino editing mode." t)
-    (require 'irony)
-    (add-to-list 'irony-supported-major-modes 'arduino-mode)
-    (add-to-list 'irony-lang-compile-option-alist '(arduino-mode . "c++"))
-    ;; Turn-on irony-mode on arduino-mode (on .ino file).
-    (add-hook 'arduino-mode-hook 'irony-mode)))
+    (use-package irony
+      :config
+      (progn
+        (add-to-list 'irony-supported-major-modes 'arduino-mode)
+        (add-to-list 'irony-lang-compile-option-alist '(arduino-mode . "c++"))
+        ;; Turn-on irony-mode on arduino-mode (on .ino file).
+        (add-hook 'arduino-mode-hook 'irony-mode)))))
