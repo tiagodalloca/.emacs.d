1871710f20ba609b163776b4aeabf3ccf298d80c
Tiago Dall'Oca <tdalloca@gmail.com>
2016-11-14 00:51:46 -0200

    Emacs additions

--- init.el
+++ init.el
@@ -65,3 +65,18 @@ locate PACKAGE."
 (setq package-enable-at-startup nil)
 
 (load-directory (from-emacsd "init/"))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(initial-frame-alist (quote ((fullscreen . maximized))))
+ '(package-selected-packages
+   (quote
+    (ample-theme powerline projectile multiple-cursors smex highlight-parentheses smartparens cider flycheck-irony company irony diminish))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
