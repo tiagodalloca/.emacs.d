26468895fbc5d330062efc98218a1c20b6b942bf
MrDallOca <tdalloca@gmail.com>
2017-04-14 12:15:52 -0300

    server-start added

--- init.el
+++ init.el
@@ -82,6 +82,8 @@ locate PACKAGE."
 (load-file (from-emacsd "init/misc.el"))
 (load-file (from-emacsd "init/themes.el"))
 
+(server-start)
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
