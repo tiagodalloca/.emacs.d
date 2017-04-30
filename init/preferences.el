cadde7b0efe86eccec00c5142624f5f4ed85f362
MrDallOca <tdalloca@gmail.com>
2017-04-30 15:45:45 -0300

    Inconsolata font

--- init/preferences.el
+++ init/preferences.el
@@ -20,7 +20,7 @@
 (defun load-font () 
 	(set-face-attribute 'default nil :font "Monospace-12")
 	(set-font-if-exists "Consolas-13")
-	(set-font-if-exists "Monaco-11"))
+	(set-font-if-exists "Inconsolata-13"))
 
 (when (display-graphic-p)
 	(load-font))
